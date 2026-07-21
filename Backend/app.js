446181c2d65cc7c935e7cb04f911309051e217e0
ManveerBains1000 <manveerjaat95@gmail.com>
2026-07-21 17:47:10 +0530

    fix: update CORS origin to production URL for secure access

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://blog-mint-eight.vercel.app/", 
     credentials: true
 }));
 
